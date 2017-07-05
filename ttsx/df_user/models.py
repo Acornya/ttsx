c78c9b112be7d3906b0c93f24f30646747550de9
zyp <137927718@qq.com>
2017-07-05 08:40:37 +0800

    模板定型1

--- ttsx/df_user/models.py
+++ ttsx/df_user/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class UserInfo(models.Model):
+    uname = models.CharField(max_length=20)
+    upwd = models.CharField(max_length=20)
+    uphone = models.IntegerField()
+    uemail = models.CharField(max_length=20)
+
+    def __str__(self):
+        return "%d" % self.pk
+
+
+class HeroInfo(models.Model):
+    hname = models.CharField(max_length=20)
+    hgender = models.BooleanField()
+    hcontent = models.CharField(max_length=100)
+    hBook = models.ForeignKey('BookInfo')
+    def __str__(self):
+        return "%d" % self.pk
--- ttsx/ttsx/urls.py
+++ ttsx/ttsx/urls.py
@@ -18,4 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^usr/', include('df_user.urls')),
+
 ]
